fd139d6fb2bf838c806b1144efaa8575b7fbf709
kulakovaekaterinapb <ekaterina.kulakova@privatbank.ua>
2021-10-30 20:18:39 +0300

    init commit

--- src/com/pb/kulakova/hw4/Anagram.java
+++ src/com/pb/kulakova/hw4/Anagram.java
@@ -4,19 +4,19 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Anagram {
-    static boolean isAnagram(String string1, String string2)
+    static boolean isAnagram(String s1, String s2)
     {
-        if(string1.length() != string2.length()) {
+        if(s1.length() != s2.length()) {
             return false;
         }
         int flag=0;
         HashMap<Character,Integer> table = new HashMap<Character,Integer>();
-        for(int i=0; i< string1.length();i++) {
-            table.put(string1.charAt(i),1);
+        for(int i=0; i< s1.length();i++) {
+            table.put(s1.charAt(i),1);
         }
 
-        for(int i=0; i< string2.length();i++) {
-            if(table.containsKey(string2.charAt(i))) {
+        for(int i=0; i< s2.length();i++) {
+            if(table.containsKey(s2.charAt(i))) {
                 continue;
             } else {
                 flag=1;
@@ -29,13 +29,13 @@ public class Anagram {
     public static void main(String[] args) {
         System.out.println("Введите выражение:");
         Scanner sc= new Scanner(System.in);
-        String string1= sc.nextLine();
-        String string2= sc.nextLine();
-        string1=string1.toLowerCase();
-        string2=string2.toLowerCase();
+        String s1= sc.nextLine();
+        String s2= sc.nextLine();
+        s1=s1.toLowerCase().replaceAll("[^А-Яа-я]","");
+        s2=s2.toLowerCase().replaceAll("[^А-Яа-я]","");
 
 
-        boolean result = isAnagram(string1,string2);
+        boolean result = isAnagram(s1,s2);
         if(result) {
             System.out.println("Это Анаграмма");
         } else{
